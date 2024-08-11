91cb36c438fd8dd200dad7e746120f8847ce97f4
wirys <wirysdacunha@outlook.com>
2024-08-10 22:49:08 -0300

    ✨ Feat: implemented Search bar

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,14 +1,42 @@
+"use client"
+import { z } from "zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Search } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
-export function SearchInput() {
+// Definindo o schema de validação com Zod
+const searchSchema = z.object({
+  query: z.string(),
+});
+
+// Definindo o tipo dos dados do formulário com base no schema
+type SearchFormData = z.infer<typeof searchSchema>;
+
+type SearchInputProps = {
+  onSearch: (query: string) => void;
+};
+
+export function SearchInput({ onSearch }: SearchInputProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SearchFormData>({
+    resolver: zodResolver(searchSchema),
+  });
+
+  const onSubmit: SubmitHandler<SearchFormData> = (data) => {
+    onSearch(data.query);
+  };
+
   return (
-    <div className="flex p-2 gap-2">
-      <Input placeholder="Buscar" />
-      <Button size={"icon"}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex p-2 gap-2">
+      <Input placeholder="Buscar" {...register("query")} />
+      <Button type="submit" size={"icon"}>
         <Search />
       </Button>
-    </div>
+    </form>
   );
 }
